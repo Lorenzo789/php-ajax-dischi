9726ed5f6de5cc1e2fc4d905e99cfae6354fe317
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-25 14:34:31 +0200

    created array album

---
+++ first-version/db/db.php
@@ -0,0 +1,74 @@
+<?php 
+
+$discs = [
+    [
+        'poster:' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-K8IqHOyCJ5gPeWVfV6pse1Q7lUL9fBahrwzukwU&usqp=CAE&s',
+        'title:' => 'The Island Chainsaw Massacre',
+        'author:' => 'Salmo',
+        'genre:' => 'Rap',
+        'year:' => '2011',
+    ],
+    [
+        'poster:' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCsclvjbn-YN3wep2x_hsFB_uC-PoIFm_4dLQBTwM&usqp=CAE&s',
+        'title:' => 'Playlist',
+        'author:' => 'Salmo',
+        'genre:' => 'Rap',
+        'year:' => '2018',
+    ],
+    [
+        'poster:' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSucwO0cPeqma2T2-ibWtkDbeAJ3JO8dinOvRXlmug&usqp=CAE&s',
+        'title:' => 'Perception',
+        'author:' => 'NF',
+        'genre:' => 'Hip Hop',
+        'year:' => '2017',
+    ],
+    [
+        'poster:' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiV0GFF-MHXSUQbcwtTMbieXhS8Qu0x0jXfZcGGh0&usqp=CAE&s',
+        'title:' => 'The Search',
+        'author:' => 'NF',
+        'genre:' => 'Hip Hop',
+        'year:' => '2019',
+    ],
+    [
+        'poster:' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg',
+        'title:' => 'Brave new World',
+        'author:' => "Iron Maiden",
+        'genre:' => "Metal",
+        'year:' => "2000",
+    ],
+    [
+        'poster:' => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+        'title:' => "Steve Gadd Band",
+        'author:' => "Steve Gadd Band",
+        'genre:' => "Jazz",
+        'year:' => "2018",
+    ],
+    [
+        'poster:' => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+        'title:' => "One more car, one more raider",
+        'author:' => "Eric Clapton",
+        'genre:' => "Rock",
+        'year:' => "2002",
+    ],
+    [
+        'poster:' => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+        'title:' => "Made in Japan",
+        'author:' => "Deep Purple",
+        'genre:' => "Rock",
+        'year:' => "1972",
+    ],
+    [
+        'poster:' => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+        'title:' => "And Justice for All",
+        'author:' => "Metallica",
+        'genre:' => "Metal",
+        'year:' => "1988",
+    ],
+    [
+        'poster:' => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+        'title:' => "Hard Wired",
+        'author:' => "Dave Weckl",
+        'genre:' => "Jazz",
+        'year:' => "1994",
+    ],
+];
