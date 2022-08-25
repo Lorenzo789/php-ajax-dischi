7f85ed30928f94f3851d9d32f64d81a7449f1dba
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-25 17:55:27 +0200

    fix

--- second-version/js/main.js
+++ second-version/js/main.js
@@ -5,6 +5,22 @@ const root = new Vue(
             listDisc: [],
         },
 
+
+        methods: {
+            getDiscs: function () {
+                axios.get('http://localhost/CORSO/Repository/php-ajax-dischi/second-version/controller/controller.php')
+                    .then( (response) => {
+
+                        this.listDisc = response.data;
+
+                        this.filteredCardGenre = this.listDisc;
+
+                        console.log(this.listDisc);
+
+                    });
+            },
+        },
+        
         created(){
             this.getDiscs();
         }
