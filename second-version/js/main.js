325e4a2483eab1c1047f1c64d0b37549acc81c71
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-25 16:27:47 +0200

    data and function axios

--- second-version/js/main.js
+++ second-version/js/main.js
@@ -1,5 +1,27 @@
 const root = new Vue(
     {
         el: '#root',
+        data: {
+            listDisc: [],
+        },
+
+
+        methods: {
+            getDiscs: function () {
+                axios.get('http://localhost/CORSO/Repository/php-ajax-dischi/second-version/controller/controller.php')
+                    .then( (response) => {
+
+                        this.listDisc = response.data;
+
+                        console.log(this.listDisc);
+
+                    });
+            }
+        },
+
+
+        created(){
+            this.getDiscs();
+        }
     },
 );
